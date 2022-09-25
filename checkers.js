ce4dbf571b42663653704391f0cbfc639fab85fc
imnadya <111196866+imnadya@users.noreply.github.com>
2022-09-25 18:44:47 +0300

    Create checkers.js

---
+++ checkers.js
@@ -0,0 +1,13 @@
+let a=55;
+let b=66;
+let output;
+if (a>b) {
+output = 'a is greater then b'}
+  else if (a==b){
+  output='a equal to b'}
+  else if (a<b) {
+    output ='a is less then b'
+  }
+  else if (a!=b){
+  output = 'N/A'}
+console.log(output);
