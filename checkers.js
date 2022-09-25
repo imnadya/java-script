66593090624de66cd98e066b40f5ccbc123bee87
imnadya <111196866+imnadya@users.noreply.github.com>
2022-09-25 19:04:28 +0300

    Update checkers.js

--- checkers.js
+++ checkers.js
@@ -1,13 +1,21 @@
-let a=55;
-let b=66;
+//If a is greater than b return 'a is greater than b' else 'a is less than b'. 
+//let a=62;
+let a = prompt("Please enter value of a");
+let b = prompt("Please enter value of b");
+// let b=66;
 let output;
 if (a>b) {
 output = 'a is greater then b'}
   else if (a==b){
-  output='a equal to b'}
+      output='a equal to b'}
   else if (a<b) {
     output ='a is less then b'
   }
   else if (a!=b){
   output = 'N/A'}
-console.log(output);
+// console.log(a);
+// console.log(b);
+// console.log(output);
+alert('a = ' + a + ', b = ' + b + " and " + output);
+
+//check whether a number is negative, positive, or zero.
